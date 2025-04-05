35d36bd0397d8e74cafa1c86d0a65c63eb8e618b
Zarahji <chukolfatima@gmail.com>
2025-04-05 22:45:45 +0100

    Hero.css added

--- foodrescue/src/Components/Navbar.jsx
+++ foodrescue/src/Components/Navbar.jsx
@@ -1,23 +1,34 @@
-// src/Components/Navbar.jsx
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assets/logo.png';
 import './Navbar.css';
+
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="logo-container">
-      <img src="/logo.png" alt="FoodRescue Logo" className="navbar-logo" />
-      className="navbar-logo" /
+    <div>
+      {/* Hero Section */}
+      <section className="hero" style={{ backgroundImage: `url(${require('../assets/hero-image.jpg')})` }}>
+        <div className="hero-content">
+          <h1>HELP END FOOD WASTE</h1>
+          <Link to="/get-started">
+            <button className="get-started-btn">Get Started</button>
+          </Link>
+        </div>
+      </section>
 
-      </div>
-      <ul className="navbar-list">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/donate">Donate</Link></li>
-        <li><Link to="/inventory">Inventory</Link></li>
-      </ul>
-    </nav>
+      {/* Navbar */}
+      <nav className="navbar">
+        <div className="logo-container">
+          <img src={logo} alt="FoodRescue Logo" className="navbar-logo" />
+        </div>
+        <ul className="navbar-list">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/dashboard">Dashboard</Link></li>
+          <li><Link to="/donate">Donate</Link></li>
+          <li><Link to="/inventory">Inventory</Link></li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
